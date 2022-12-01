3459f265602e0c977584d8771e3e95707b20d20b
taryune <taryunelab@gmail.com>
2022-12-01 22:02:17 +0100

    add createGame

--- x/checkers/keeper/msg_server_create_game.go
+++ x/checkers/keeper/msg_server_create_game.go
@@ -2,7 +2,10 @@ package keeper
 
 import (
 	"context"
+	"errors"
+	"strconv"
 
+	"github.com/alice/checkers/x/checkers/rules"
 	"github.com/alice/checkers/x/checkers/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
@@ -10,12 +13,34 @@ import (
 func (k msgServer) CreateGame(goCtx context.Context, msg *types.MsgCreateGame) (*types.MsgCreateGameResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
 	red, err := sdk.AccAddressFromBech32(msg.Red)
 	if err != nil {
-		return nil, error.New('invalid address for red')
+		return nil, errors.New("invalid address for red")
+	}
+	black, err := sdk.AccAddressFromBech32(msg.Black)
+	if err != nil {
+		return nil, errors.New("invalid address for Black")
 	}
 
-	return &types.MsgCreateGameResponse{}, nil
+	systemInfo, found := k.Keeper.GetSystemInfo(ctx)
+	if !found {
+		panic("systemInfo not found")
+	}
+	newIndex := strconv.FormatUint(systemInfo.NextId, 10)
+
+	storedGame := types.StoredGame{
+		Index: newIndex,
+		Board: rules.New().String(),
+		Turn:  red.String(),
+		Black: black.String(),
+		Red:   red.String(),
+	}
+
+	k.Keeper.SetStoredGame(ctx, storedGame)
+	systemInfo.NextId++
+	k.Keeper.SetSystemInfo(ctx, systemInfo)
+
+	return &types.MsgCreateGameResponse{
+		GameIndex: newIndex,
+	}, nil
 }
