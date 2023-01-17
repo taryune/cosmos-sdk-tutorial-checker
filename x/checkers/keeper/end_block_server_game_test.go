63d99438adfa430e730ef1874666ece76f231832
taryune <taryunelab@gmail.com>
2023-01-17 13:23:23 +0100

    impl auto expire

---
+++ x/checkers/keeper/end_block_server_game_test.go
@@ -0,0 +1,82 @@
+package keeper_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/alice/checkers/x/checkers/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/require"
+)
+
+func TestForfeitUnplayed(t *testing.T) {
+	_, keeper, context := setupMsgServerWithOneGameForPlayMove(t)
+	ctx := sdk.UnwrapSDKContext(context)
+	game1, found := keeper.GetStoredGame(ctx, "1")
+	require.True(t, found)
+	game1.Deadline = types.FormatDeadline(ctx.BlockTime().Add(time.Duration(-1)))
+	keeper.SetStoredGame(ctx, game1)
+	keeper.ForfeitExpiredGames(context)
+
+	_, found = keeper.GetStoredGame(ctx, "1")
+	require.False(t, found)
+
+	systemInfo, found := keeper.GetSystemInfo(ctx)
+	require.True(t, found)
+	require.EqualValues(t, types.SystemInfo{
+		NextId:        2,
+		FifoHeadIndex: "-1",
+		FifoTailIndex: "-1",
+	}, systemInfo)
+	events := sdk.StringifyEvents(ctx.EventManager().ABCIEvents())
+	require.Len(t, events, 2)
+	event := events[0]
+	require.EqualValues(t, sdk.StringEvent{
+		Type: "game-forfeited",
+		Attributes: []sdk.Attribute{
+			{Key: "game-index", Value: "1"},
+			{Key: "winner", Value: "*"},
+			{Key: "board", Value: "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*"},
+		},
+	}, event)
+}
+
+func TestForfeitPlayedOnce(t *testing.T) {
+	msgServer, keeper, context := setupMsgServerWithOneGameForPlayMove(t)
+	ctx := sdk.UnwrapSDKContext(context)
+	msgServer.PlayMove(context, &types.MsgPlayMove{
+		Creator:   bob,
+		GameIndex: "1",
+		FromX:     1,
+		FromY:     2,
+		ToX:       2,
+		ToY:       3,
+	})
+	game1, found := keeper.GetStoredGame(ctx, "1")
+	require.True(t, found)
+	game1.Deadline = types.FormatDeadline(ctx.BlockTime().Add(time.Duration(-1)))
+	keeper.SetStoredGame(ctx, game1)
+	keeper.ForfeitExpiredGames(context)
+
+	_, found = keeper.GetStoredGame(ctx, "1")
+	require.False(t, found)
+
+	systemInfo, found := keeper.GetSystemInfo(ctx)
+	require.True(t, found)
+	require.EqualValues(t, types.SystemInfo{
+		NextId:        2,
+		FifoHeadIndex: "-1",
+		FifoTailIndex: "-1",
+	}, systemInfo)
+	events := sdk.StringifyEvents(ctx.EventManager().ABCIEvents())
+	require.Len(t, events, 3)
+	event := events[0]
+	require.EqualValues(t, sdk.StringEvent{
+		Type: "game-forfeited",
+		Attributes: []sdk.Attribute{
+			{Key: "game-index", Value: "1"},
+			{Key: "winner", Value: "*"},
+			{Key: "board", Value: "*b*b*b*b|b*b*b*b*|***b*b*b|**b*****|********|r*r*r*r*|*r*r*r*r|r*r*r*r*"},
+		},
+	}, event)
+}
